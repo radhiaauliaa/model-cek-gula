67b09eb5fec4a322b2a637fa2332f4f6990740dd
Radhia Aulia Nisa <radhiaaulianisa@gmail.com>
2026-05-26 10:09:20 +0700

    deploy huggingface

--- groq_helper.py
+++ groq_helper.py
@@ -1,13 +1,9 @@
 import os
-from dotenv import load_dotenv
 from groq import Groq
-
-load_dotenv()
 
 client = Groq(
     api_key=os.getenv("GROQ_API_KEY")
 )
-
 
 def get_rekomendasi(
     prediksi_label,
